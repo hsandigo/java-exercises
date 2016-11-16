b7e63b1a250b10cd4fd3e93f004000e8213a55f2
Humberto Sandigo <sandigohumberto@gmail.com>
2016-11-16 10:36:25 -0600

    editing code with functional code

--- src/ExceptionsAndErrorHandling.java
+++ src/ExceptionsAndErrorHandling.java
@@ -1,20 +1,38 @@
 /*Created by humbertosandigo on 11/15/16.*/
 
+import sun.nio.ch.sctp.SctpNet;
+
 import java.util.Scanner;
 public class ExceptionsAndErrorHandling {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Student Name: ");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter Student Name: ");
+//
+//        System.out.println("Enter Student Age: ");
+//        Integer input = sc.nextInt();
+//
+//
+//        if(input <= 0 || input > 100){
+//            System.out.println("The input is not valid");
+//        }
+        String studentName;
+        int age;
+        String reason;
 
-        System.out.println("Enter Student Age: ");
-        Integer input = sc.nextInt();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        studentName = input.nextLine();
+
+        System.out.println("What's your age?");
+        age = input.nextInt();
+
+        System.out.println("Why do you want to learn to program?");
+        reason = input.nextLine();
 
 
-        if(input <= 0 || input > 100){
-            System.out.println("The input is not valid");
-        }
-
-
-
+        System.out.println("Student name is "+ studentName);
+        System.out.println("Student's age is " + age);
+        System.out.println("Student wants to learn to code because  " + reason);
     }
 }
